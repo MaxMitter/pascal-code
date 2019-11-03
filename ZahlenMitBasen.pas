f6b056ee03c68bbedeffb5c8c4988bcf6a15cabd
unknown <max.mitter14@gmail.com>
2019-11-03 19:09:51 +0100

    Finished main function for homework nr 4

--- ZahlenMitBasen.pas
+++ ZahlenMitBasen.pas
@@ -129,33 +129,114 @@ PROGRAM ZahlenMitBasen;
   END; (* Quot *)
 
   var number: string;
+  var numberInt: integer;
   var base: integer;
 
   var v1, v2: string;
   var b1, b2: integer;
 
+  var input: integer;
+
 BEGIN (* ZahlenMitBasen *)
-  (*Write('Zahl: ');
-  ReadLn(number);
-  Write('Basis: ');
-  ReadLn(base);
+  WriteLn('Waehlen Sie die gewuenschte Funktion aus:');
+  WriteLn('0: Beendet das Programm.');
+  WriteLn('1: Zahl mit beliebiger Basis zwischen 2 und 36 ins Dezimalsystem umrechnen.');
+  WriteLn('2: Zahl aus dem Dezimalsystem in eine Zahl mit beliebiger Basis zwischen 2 und 36 umrechnen.');
+  WriteLn('3: Zwei Zahlen mit beliebiger Basis zwischen 2 und 36 addieren.');
+  WriteLn('4: Zwei Zahlen mit beliebiger Basis zwischen 2 und 36 subtrahieren.');
+  WriteLn('5: Zwei Zahlen mit beliebiger Basis zwischen 2 und 36 multiplizieren.');
+  WriteLn('6: Zwei Zahlen mit beliebiger Basis zwischen 2 und 36 dividieren.');
+  Write('Eingabe: ');
+  ReadLn(input);
 
-  Write(ValueOf(number, base), ', ');
-  Write(DigitsOf(ValueOf(number, base), base)); *)
+  WHILE (input <> 0) DO BEGIN
 
-  Write('Zahl 1: ');
-  ReadLn(v1);
-  Write('Basis 1: ');
-  ReadLn(b1);
-  Write('Zahl 2: ');
-  ReadLn(v2);
-  Write('Basis 2: ');
-  ReadLn(b2);
+    IF (input = 1) THEN BEGIN
+      Write('Zahl: ');
+      Read(number);
+      Write('Basis: ');
+      Read(base);
 
-  IF (b1 = b2) THEN BEGIN
-    Write('Ergebnis: ', DigitsOf(Quot(v1, b1, v2, b2), b1), ' (Basis: ', b1, ')');
-  END ELSE BEGIN
-      Write('Ergebnis: ', Quot(v1, b1, v2, b2), ' (Basis: 10)');
-  END; (* IF *)
-  
+      WriteLn('Zahl im Dezimalsystem: ', ValueOf(number, base));
+
+    END ELSE IF (input = 2) THEN BEGIN
+      Write('Zahl: ');
+      Read(numberInt);
+      Write('Basis: ');
+      Read(base);
+
+      WriteLn('Zahl mit Basis ', base, ': ', DigitsOf(numberInt, base));
+
+      END ELSE IF (input = 3) THEN BEGIN
+        Write('Zahl 1: ');
+        Read(v1);
+        Write('Basis 1: ');
+        ReadLn(b1);
+        Write('Zahl 2: ');
+        Read(v2);
+        Write('Basis 2: ');
+        Read(b2);
+
+        IF (b1 = b2) THEN BEGIN
+          WriteLn('Ergebnis: ', DigitsOf(Sum(v1, b1, v2, b2), b1), ' (Basis: ', b1, ')');
+        END ELSE BEGIN
+            WriteLn('Ergebnis: ', Sum(v1, b1, v2, b2), ' (Basis: 10)');
+        END; (* IF *)
+
+      END ELSE IF (input = 4) THEN BEGIN
+        Write('Zahl 1: ');
+        Read(v1);
+        Write('Basis 1: ');
+        ReadLn(b1);
+        Write('Zahl 2: ');
+        Read(v2);
+        Write('Basis 2: ');
+        Read(b2);
+
+        IF (b1 = b2) THEN BEGIN
+          WriteLn('Ergebnis: ', DigitsOf(Diff(v1, b1, v2, b2), b1), ' (Basis: ', b1, ')');
+        END ELSE BEGIN
+            WriteLn('Ergebnis: ', Diff(v1, b1, v2, b2), ' (Basis: 10)');
+        END; (* IF *)
+
+      END ELSE IF (input = 5) THEN BEGIN
+        Write('Zahl 1: ');
+        Read(v1);
+        Write('Basis 1: ');
+        ReadLn(b1);
+        Write('Zahl 2: ');
+        Read(v2);
+        Write('Basis 2: ');
+        Read(b2);
+
+        IF (b1 = b2) THEN BEGIN
+          WriteLn('Ergebnis: ', DigitsOf(Prod(v1, b1, v2, b2), b1), ' (Basis: ', b1, ')');
+        END ELSE BEGIN
+            WriteLn('Ergebnis: ', Prod(v1, b1, v2, b2), ' (Basis: 10)');
+        END; (* IF *)
+
+      END ELSE IF (input = 6) THEN BEGIN
+        Write('Zahl 1: ');
+        Read(v1);
+        Write('Basis 1: ');
+        ReadLn(b1);
+        Write('Zahl 2: ');
+        Read(v2);
+        Write('Basis 2: ');
+        Read(b2);
+
+        IF (b1 = b2) THEN BEGIN
+          WriteLn('Ergebnis: ', DigitsOf(Quot(v1, b1, v2, b2), b1), ' (Basis: ', b1, ')');
+        END ELSE BEGIN
+            WriteLn('Ergebnis: ', Quot(v1, b1, v2, b2), ' (Basis: 10)');
+        END; (* IF *)
+
+        END ELSE BEGIN
+          WriteLn('Ungueltige Eingabe, Programm wird beendet.');
+          Exit();
+    END; (* IF *)
+
+    Write('Eingabe: ');
+    ReadLn(input);
+  END; (* WHILE *)
 END. (* ZahlenMitBasen *)
